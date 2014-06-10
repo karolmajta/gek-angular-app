445d1b7c15dce7f524612ad8c13536f7ce9f0a73
Karol Majta <karol@karolmajta.com>
2014-06-10 22:23:18 +0200

    Added constants with configuration

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,16 @@
 angular.module('derpErp', [])
 
-.controller('quotaInfoController', ['$scope', function ($scope) {
+.constant('API_KEY', angular.element(document.getElementsByTagName('head')).attr('data-api-key'))
+.constant('API_ROOT', 'http://gek-angular-workshop.herokuapp.com/api')
+
+.controller('quotaInfoController', [
+    '$scope', 'API_KEY', 'API_ROOT',
+    function ($scope, API_KEY, API_ROOT) {
 
   $scope.requestsPerSecond = 20;
+  $scope.configuration = {
+    API_KEY: API_KEY,
+    API_ROOT: API_ROOT
+  }
 
 }]);
